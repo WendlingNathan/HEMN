c658baa3822b417c87fc5d616fff813792464cc1
Skildim <cassol.heitor@gmail.com>
2025-06-21 16:41:48 -0300

    Anexo do select do cardapio

--- Scripts/MEHN Selects.sql
+++ Scripts/MEHN Selects.sql
@@ -8,7 +8,11 @@ select * from pedido;
 select * from produto;
 
 //Listar todos os pedidos com o nome  cliente status e total  pedido
-select cliente.id_cli,pedido.id_cli,pagamento.valor_total_pag,pagamento.id_ped,pedido.id_ped
+select cliente.id_cli,pedido.id_cli,pagamento.valor_total_pag,pagamento.id_ped,pedido.id_ped,pedido.status_ped
 from cliente
 inner join pedido on cliente.id_cli = pedido.id_cli
 inner join pagamento on pedido.id_ped = pagamento.id_ped;
+
+//Consultar o cardápio completo com nome descrição tipo e preço
+select produto.nome_prod,produto.descricao_prod,produto.preco_prod
+from produto;
