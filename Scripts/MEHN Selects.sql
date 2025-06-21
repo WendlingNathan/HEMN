e8bace3e4fd9a1545514573f76d306a47c8200a5
Skildim <cassol.heitor@gmail.com>
2025-06-21 15:42:58 -0300

    Anexo select geral do projeto

---
+++ Scripts/MEHN Selects.sql
@@ -0,0 +1,8 @@
+//GERAL TODAS TABELAS
+select * from cliente; 
+select * from endereco;
+select * from funcionario;
+select * from item;
+select * from pagamento;
+select * from pedido;
+select * from produto;
