abfb7693a24796020d7f6f7e3c2027474790efcf
Skildim <cassol.heitor@gmail.com>
2025-06-21 16:15:52 -0300

    Anexo lista todos os pedidos com o nome do cliente e seu total do pedido, com status.

--- Scripts/MEHN Selects.sql
+++ Scripts/MEHN Selects.sql
@@ -6,3 +6,9 @@ select * from item;
 select * from pagamento;
 select * from pedido;
 select * from produto;
+
+//Listar todos os pedidos com o nome  cliente status e total  pedido
+select cliente.id_cli,pedido.id_cli,pagamento.valor_total_pag,pagamento.id_ped,pedido.id_ped
+from cliente
+inner join pedido on cliente.id_cli = pedido.id_cli
+inner join pagamento on pedido.id_ped = pagamento.id_ped;
