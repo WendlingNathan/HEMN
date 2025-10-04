c855e394008a9bded8a620bd3991257bb68e27e2
marcoschons <marcoschons123@gmail.com>
2025-10-04 16:44:25 -0300

    Criação inicial de usuarios e permissões:Admin, funcionario e cliente

---
+++ database/Scripts/Script Usuários/Scrip Usuario.sql
@@ -0,0 +1,17 @@
+
+
+create role grupo_admin nologin; 
+create role grupo_funcionario nologin;
+create role grupo_clientes nologin;
+
+create user Administrator with password 'admin20';
+create user funcionario with password 'fun5420';
+create user cliente with password 'cli123';
+
+grant all privileges on all tables in schema public to grupo_admin;
+grant select, insert, update on all tables in schema public to grupo_funcionario;
+grant select on all tables in schema public to grupo_clientes;
+
+
+
+
