bfd429cfbdbb7bb9220c7c9f90e0db2b2721aa7d
marcoschons <marcoschons123@gmail.com>
2025-10-04 18:01:58 -0300

    Associação de grupos aos usuários respectivos

--- database/Scripts/Script Usuários/Scrip Usuario.sql
+++ database/Scripts/Script Usuários/Scrip Usuario.sql
@@ -1,17 +1,21 @@
 
-
-create role grupo_admin nologin; 
+-- Criando roles (grupos)
+create role grupo_admin nologin;
 create role grupo_funcionario nologin;
 create role grupo_clientes nologin;
 
+-- Criando usuários 
 create user Administrator with password 'admin20';
 create user funcionario with password 'fun5420';
 create user cliente with password 'cli123';
+
 
 grant all privileges on all tables in schema public to grupo_admin;
 grant select, insert, update on all tables in schema public to grupo_funcionario;
 grant select on all tables in schema public to grupo_clientes;
 
-
-
+-- Associando usuários aos grupos
+grant grupo_admin to administrator;
+grant grupo_funcionario to funcionario;
+grant grupo_clientes to cliente;
 
