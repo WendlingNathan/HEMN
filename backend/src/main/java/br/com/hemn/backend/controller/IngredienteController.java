f2ddc28afc04c85a2320c9f03c909b8f24e44a99
WendlingNathan <natritwen@gmail.com>
2025-11-08 23:52:39 -0300

    Classe IngredienteController criada
    Endpoints testados e aprovados

---
+++ backend/src/main/java/br/com/hemn/backend/controller/IngredienteController.java
@@ -0,0 +1,35 @@
+package br.com.hemn.backend.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.hemn.backend.model.Endereco;
+import br.com.hemn.backend.model.Ingrediente;
+import br.com.hemn.backend.repository.IngredienteRepository;
+
+@RestController
+@RequestMapping("/ingrediente")
+public class IngredienteController {
+
+	@Autowired
+	private IngredienteRepository ingredienteRepository;
+
+	@GetMapping
+	public List<Ingrediente> listarTodos() {
+		return ingredienteRepository.findAll();
+	}
+	
+    @GetMapping("/{id}")
+	public ResponseEntity<Ingrediente> buscarPorId(@PathVariable Long id) {
+		Optional<Ingrediente> ingrediente = ingredienteRepository.findById(id);
+		return ingrediente.map(ResponseEntity::ok)
+					   .orElse(ResponseEntity.notFound().build());
+	}
+}
