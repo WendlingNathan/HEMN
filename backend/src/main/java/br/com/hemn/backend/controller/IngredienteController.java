1d34ba2213c1f28c0a39b28eb262a7d679b36e35
Eduardo Nofre <nofreeduardo@gmail.com>
2025-11-09 18:59:47 -0300

    Criado tabela ComposicaoProdutoController

--- backend/src/main/java/br/com/hemn/backend/controller/IngredienteController.java
+++ backend/src/main/java/br/com/hemn/backend/controller/IngredienteController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.hemn.backend.model.Endereco;
 import br.com.hemn.backend.model.Ingrediente;
 import br.com.hemn.backend.repository.IngredienteRepository;
 
