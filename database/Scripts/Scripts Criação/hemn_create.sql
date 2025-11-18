a66f1ec0ea8f88e1fe6c197d1b9bfd5187d854f9
WendlingNathan <natritwen@gmail.com>
2025-11-17 21:15:58 -0300

    Auditoria: Feita adição das tabelas 'Aud_pedido' e 'Aud_produto_preco'
    Adição feita para comportar triggers e funções para uma possível auditoria

--- database/Scripts/Scripts Criação/hemn_create.sql
+++ database/Scripts/Scripts Criação/hemn_create.sql
@@ -12,6 +12,7 @@ COMMENT ON COLUMN Cliente.id_cli IS 'Id do cliente';
 COMMENT ON COLUMN Cliente.nome_cli IS 'Nome do cliente';
 COMMENT ON COLUMN Cliente.cpf_cli IS 'CPF do cliente';
 COMMENT ON COLUMN Cliente.telefone_cli IS 'Telefone do cliente';
+
 CREATE TABLE ComposicaoProduto (
   id_comp      SERIAL NOT NULL, 
   qtd_ing_comp numeric(5, 2) NOT NULL, 
@@ -19,9 +20,10 @@ CREATE TABLE ComposicaoProduto (
   id_ing       int4 NOT NULL, 
   CONSTRAINT composicaoProduto_pkey 
     PRIMARY KEY (id_comp));
-COMMENT ON TABLE ComposicaoProduto IS 'Tabela da composição do produto.';
-COMMENT ON COLUMN ComposicaoProduto.id_comp IS 'ID da composição do produto.';
-COMMENT ON COLUMN ComposicaoProduto.qtd_ing_comp IS 'Quantidade do produto na composição.';
+COMMENT ON TABLE ComposicaoProduto IS 'Tabela da composiï¿½ï¿½o do produto.';
+COMMENT ON COLUMN ComposicaoProduto.id_comp IS 'ID da composiï¿½ï¿½o do produto.';
+COMMENT ON COLUMN ComposicaoProduto.qtd_ing_comp IS 'Quantidade do produto na composiï¿½ï¿½o.';
+
 CREATE TABLE Endereco (
   id_end          SERIAL NOT NULL, 
   complemento_end varchar(20) NOT NULL, 
@@ -33,13 +35,14 @@ CREATE TABLE Endereco (
   CONSTRAINT endereco_pkey 
     PRIMARY KEY (id_end));
 COMMENT ON TABLE Endereco IS 'Tabela Endereco.';
-COMMENT ON COLUMN Endereco.id_end IS 'Id do endereço';
-COMMENT ON COLUMN Endereco.complemento_end IS 'Complemento do endereço';
-COMMENT ON COLUMN Endereco.logradouro_end IS 'Logradouro do endereço';
-COMMENT ON COLUMN Endereco.numero_end IS 'Número do endereço';
-COMMENT ON COLUMN Endereco.cidade_end IS 'Cidade do endereço';
-COMMENT ON COLUMN Endereco.bairro_end IS 'Bairro do endereço';
-COMMENT ON COLUMN Endereco.pont_ref_end IS 'Ponto de referência do endereço';
+COMMENT ON COLUMN Endereco.id_end IS 'Id do endereï¿½o';
+COMMENT ON COLUMN Endereco.complemento_end IS 'Complemento do endereï¿½o';
+COMMENT ON COLUMN Endereco.logradouro_end IS 'Logradouro do endereï¿½o';
+COMMENT ON COLUMN Endereco.numero_end IS 'Nï¿½mero do endereï¿½o';
+COMMENT ON COLUMN Endereco.cidade_end IS 'Cidade do endereï¿½o';
+COMMENT ON COLUMN Endereco.bairro_end IS 'Bairro do endereï¿½o';
+COMMENT ON COLUMN Endereco.pont_ref_end IS 'Ponto de referï¿½ncia do endereï¿½o';
+
 CREATE TABLE Funcionario (
   id_fun       SERIAL NOT NULL, 
   nome_fun     varchar(80) NOT NULL, 
@@ -52,6 +55,7 @@ COMMENT ON COLUMN Funcionario.id_fun IS 'Id do funcionario';
 COMMENT ON COLUMN Funcionario.nome_fun IS 'Nome do funcionario';
 COMMENT ON COLUMN Funcionario.cargo_fun IS 'Cargos dos Funcionarios (''C'' - "Cozinheiro", ''A'' - "Atentende", ''E'' - "Entregador")';
 COMMENT ON COLUMN Funcionario.telefone_fun IS 'Telefone do funcionario';
+
 CREATE TABLE Ingrediente (
   id_ing             SERIAL NOT NULL, 
   nome_ing           varchar(60) NOT NULL UNIQUE, 
@@ -62,6 +66,7 @@ COMMENT ON TABLE Ingrediente IS 'Tabela do Ingrediente.';
 COMMENT ON COLUMN Ingrediente.id_ing IS 'ID do Ingrediente.';
 COMMENT ON COLUMN Ingrediente.nome_ing IS 'Nome do Ingrediente.';
 COMMENT ON COLUMN Ingrediente.unidade_medida_ing IS 'Unidade de medida ("G" - "Grama", "ML" - "Mililitro", "UN" - "Unidade").';
+
 CREATE TABLE Item (
   id_ite             SERIAL NOT NULL, 
   qtd_ite            int4 NOT NULL, 
@@ -72,7 +77,8 @@ CREATE TABLE Item (
     PRIMARY KEY (id_ite));
 COMMENT ON TABLE Item IS 'Tabela Item - Representa cada item do pedido.';
 COMMENT ON COLUMN Item.qtd_ite IS 'Quantidade de itens do pedido';
-COMMENT ON COLUMN Item.preco_unitario_ite IS 'preço unitario do item';
+COMMENT ON COLUMN Item.preco_unitario_ite IS 'preï¿½o unitario do item';
+
 CREATE TABLE Pagamento (
   id_pag             SERIAL NOT NULL, 
   valor_total_pag    numeric(4, 2) NOT NULL, 
@@ -84,8 +90,9 @@ CREATE TABLE Pagamento (
 COMMENT ON TABLE Pagamento IS 'Tabela Pagamento.';
 COMMENT ON COLUMN Pagamento.id_pag IS 'id do pagamento';
 COMMENT ON COLUMN Pagamento.valor_total_pag IS 'Valor total do pagamento';
-COMMENT ON COLUMN Pagamento.forma_pagameto_pag IS 'Forma de pagamento (''D'' - "Dinheiro",''C'' - "Cartão",''P'' - "Pix")';
+COMMENT ON COLUMN Pagamento.forma_pagameto_pag IS 'Forma de pagamento (''D'' - "Dinheiro",''C'' - "Cartï¿½o",''P'' - "Pix")';
 COMMENT ON COLUMN Pagamento.data_pag IS 'Data de pagamento';
+
 CREATE TABLE Pedido (
   id_ped        SERIAL NOT NULL, 
   data_hora_ped timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL, 
@@ -98,6 +105,38 @@ COMMENT ON TABLE Pedido IS 'Tabela Pedido.';
 COMMENT ON COLUMN Pedido.id_ped IS 'Id do pedido';
 COMMENT ON COLUMN Pedido.data_hora_ped IS 'Data e hora do pedido';
 COMMENT ON COLUMN Pedido.status_ped IS 'Status do pedido (''A'' - "Aguarde", ''P'' - "Pronto", ''E'' - "Entregue")';
+
+-- Tabelas para uma auditoria
+CREATE TABLE IF NOT EXISTS Aud_pedido (
+  id_aud        BIGSERIAL PRIMARY KEY,
+  id_ped        INT NOT NULL,
+  operacao      VARCHAR(10) NOT NULL, -- 'UPDATE' / 'DELETE' / 'INSERT' (se desejar)
+  dados_anteriores JSONB,
+  dados_novos      JSONB,
+  usuario       TEXT DEFAULT current_user,
+  data_operacao  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+COMMENT ON TABLE Aud_pedido IS 'Auditoria de alteraÃ§Ãµes na tabela Pedido (Antigo/Novo).';
+COMMENT ON COLUMN Aud_pedido.operacao IS 'OperaÃ§Ã£o feita pelo usuÃ¡rio (UPDATE, DELETE, INSERT)';
+COMMENT ON COLUMN Aud_pedido.dados_anteriores IS 'Dados anteriores decorrentes da operaÃ§Ã£o';
+COMMENT ON COLUMN Aud_pedido.dados_novos IS 'Dados movos decorrentes da operaÃ§Ã£o';
+COMMENT ON COLUMN Aud_pedido.usuario IS 'UsuÃ¡rio que executou a operaÃ§Ã£o';
+COMMENT ON COLUMN Aud_pedido.data_operacao IS 'Data da execuÃ§Ã£o da operaÃ§Ã£o';
+
+CREATE TABLE IF NOT EXISTS Aud_produto_preco (
+  id_aud        BIGSERIAL PRIMARY KEY,
+  id_prod       INT NOT NULL,
+  preco_antigo  NUMERIC(12,2),
+  preco_novo    NUMERIC(12,2),
+  usuario       TEXT DEFAULT current_user,
+  data_operacao TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+COMMENT ON TABLE Aud_produto_preco IS 'HistÃ³rico de alteraÃ§Ã£o de preÃ§os na tabela Produto.';
+COMMENT ON COLUMN Aud_produto_preco.preco_antigo IS 'PreÃ§o antigo do produto respectivo';
+COMMENT ON COLUMN Aud_produto_preco.preco_novo IS 'PreÃ§o novo do produto respectivo';
+COMMENT ON COLUMN Aud_produto_preco.usuario IS 'UsuÃ¡rio que executou a operaÃ§Ã£o';
+COMMENT ON COLUMN Aud_produto_preco.data_operacao IS 'Data da execuÃ§Ã£o da operaÃ§Ã£o';
+
 CREATE TABLE Produto (
   id_prod        SERIAL NOT NULL, 
   nome_prod      varchar(40) NOT NULL, 
@@ -108,9 +147,10 @@ CREATE TABLE Produto (
     PRIMARY KEY (id_prod));
 COMMENT ON TABLE Produto IS 'Tabela Produto.';
 COMMENT ON COLUMN Produto.nome_prod IS 'Nome do produto';
-COMMENT ON COLUMN Produto.descricao_prod IS 'Descrição do produto';
-COMMENT ON COLUMN Produto.preco_prod IS 'Preço do produto';
+COMMENT ON COLUMN Produto.descricao_prod IS 'Descriï¿½ï¿½o do produto';
+COMMENT ON COLUMN Produto.preco_prod IS 'Preï¿½o do produto';
 COMMENT ON COLUMN Produto.tipo_prod IS 'Tipo de produto (''L'' - "Lanche", ''A'' - "Acompanhamento", ''B'' - "Bebida")';
+
 ALTER TABLE Cliente ADD CONSTRAINT cliente_id_end_fkey_001 FOREIGN KEY (id_end) REFERENCES Endereco (id_end) ON UPDATE Cascade ON DELETE Restrict;
 ALTER TABLE ComposicaoProduto ADD CONSTRAINT composicaoProduto_id_ing_fkey_002 FOREIGN KEY (id_ing) REFERENCES Ingrediente (id_ing) ON UPDATE Cascade ON DELETE Restrict;
 ALTER TABLE ComposicaoProduto ADD CONSTRAINT composicaoProduto_id_prod_fkey_001 FOREIGN KEY (id_prod) REFERENCES Produto (id_prod) ON UPDATE Cascade ON DELETE Restrict;
