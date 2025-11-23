71c662de18b5e6f3d745fa71365f780c517a02e1
WendlingNathan <natritwen@gmail.com>
2025-11-22 22:57:46 -0300

    JavaDoc: feita a documentação da classe CardapioController e atuzalizada o JavaDoc geral

--- backend/src/main/java/br/com/hemn/backend/controller/CardapioController.java
+++ backend/src/main/java/br/com/hemn/backend/controller/CardapioController.java
@@ -7,15 +7,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import br.com.hemn.backend.model.Cliente;
 import br.com.hemn.backend.model.Produto;
 import br.com.hemn.backend.repository.ProdutoRepository;
 
+/**
+ * Controlador responsável por gerenciar a operação relacionada à exibição do cardápio no frontend {@link Cliente}.
+ * <p>
+ * Disponibiliza um endpoint para listagem e consulta dos produtos registrados no sistema, mostrando-os no frontend.
+ * </p>
+ *
+ * @author Nathan Ritter Wendling
+ * @author Eduardo Augusto Romio Nofre
+ * @author Marco Antônio Schons Santos
+ */
 @Controller
 public class CardapioController {
 
+	/** Repositório da entidade Produto. */
     @Autowired
     private ProdutoRepository produtoRepository;
 
+    /**
+     * Faz uma checagem e gera uma lista com todos os produtos cadastrados.
+     *
+     * @return Aciona a página do cardápio, listando todos os {@link Produtos} presentes no banco de dados.
+     */
     @GetMapping("/cardapio")
     public String mostrarCardapio(Model model) {
         List<Produto> produtos = produtoRepository.findAll();
